5c37fc78de9ac989e60ab91ba6d7f784f14c6387
Alexei Razoumov <alex.razoumov@westgrid.ca>
2019-05-17 21:00:04 -0400

    2019-May-17 21:00

--- codes/baseSolver.sh
+++ codes/baseSolver.sh
@@ -2,4 +2,7 @@
 #SBATCH --time=00:05:00   # walltime in d-hh:mm or hh:mm:ss format
 #SBATCH --mem-per-cpu=1000   # in MB
 #SBATCH --output=baseSolver.out
-./baseSolver -nl 1 --rows=650 --cols=650 --niter=9000 --tolerance=0.002
+
+#./baseSolver -nl 1 --rows=30 --cols=30 --niter=2000
+./baseSolver -nl 1 --rows=650 --cols=650 --niter=9500 --tolerance=0.002
+# ./baseSolver -nl 1 --rows=2000 --cols=2000 --niter=9500 --tolerance=0.002
--- codes/parallel3.sh
+++ codes/parallel3.sh
@@ -4,4 +4,7 @@
 #SBATCH --nodes=4
 #SBATCH --cpus-per-task=2
 #SBATCH --output=parallel3.out
-./parallel3 -nl 4 --rows=650 --cols=650 --niter=9000 --tolerance=0.002
+
+#./parallel3 -nl 4 --rows=30 --cols=30 --niter=2000
+./parallel3 -nl 4 --rows=650 --cols=650 --niter=9500 --tolerance=0.002
+# ./parallel3 -nl 4 --rows=2000 --cols=2000 --niter=9500 --tolerance=0.002
